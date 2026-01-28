13fe8b2d1b7bac53d54e7033451af71e04d96db5
Dang Nguyen Truc Vi <kimnguyen1236@gmail.com>
2026-01-28 16:51:15 +0700

    Rotating our Player using the AddTorque() method

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,30 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     InputAction moveAction;
+    RigidBody2D myRigidBody2D;
+    [SerializeField] float torqueAmount = 1f;
+
     
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
-
+        myRigidBody2D = GetComponent<RigidBody2D>();
     }
     
     void Update()
     {
         Vector2 moveVector;
         moveVector = moveAction.ReadValue<Vector2>();
-        print(moveVector);
+        myRigidBody2D.AddTorque(torqueAmount);
+        
+        if(moveVector.x < 0)
+        {
+            myRigidbody2D.AddTorque(torqueAmount);
+        }
+        
+        else if(moveVector.x > 0)
+        {
+            myRigidbody2D.AddTorque(torqueAmount*-1);
+        }
     }
 }
