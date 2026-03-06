341e6ce475d8c6ff39fd2be490fd520dc552491b
Dang Nguyen Truc Vi <kimnguyen1236@gmail.com>
2026-03-06 15:17:08 +0700

    Using FindFirstObjectOfType() to speed up when the player press up.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,29 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {    
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float baseSpeed = 15f;
+    [SerializeField] float boostSpeed = 20f;
     InputAction moveAction;
     Rigidbody2D myRigidbody2D;
+    Vector2 moveVector;
+    SurfaceEffector2D surfaceEffector2D;
     
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         myRigidbody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindFirstObjectByType<SurfaceEffector2D>();
     }
     
     void Update()
     {
-        Vector2 moveVector;
+        RotatePlayer();
+        BoostPlayer();
+
+    }
+
+    void RotatePlayer()
+    {
         moveVector = moveAction.ReadValue<Vector2>();
         
         if(moveVector.x < 0)
@@ -28,6 +39,18 @@ public class PlayerController : MonoBehaviour
         else if(moveVector.x > 0)
         {
             myRigidbody2D.AddTorque(-torqueAmount);
+        }
+    }
+
+    void BoostPlayer()
+    {
+        if (moveVector.y > 0)
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
         }
     }
 }
